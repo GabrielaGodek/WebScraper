dc44161d3266c3281a77952355a06b30f63ad7f6
GabrielaGodek <godekgabriela39@gmail.com>
2022-12-20 23:17:29 +0100

    refactoring

---
+++ data.py
@@ -0,0 +1,36 @@
+import json_lines
+import matplotlib.pyplot as plt
+
+eur_pln, usd_pln, eur_usd  = [], [], []
+
+# Get the data from API
+with open('./data/currency.jl', 'rb') as data_file:
+    for item in json_lines.reader(data_file):
+        if item['title'] == 'EUR/PLN':
+            eur_pln.append(item)
+        elif item['title'] == 'EUR/USD':
+            eur_usd.append(item)
+        elif item['title'] == 'USD/PLN':
+            usd_pln.append(item)
+            
+def makeChart(tmp):
+    tmp_value, tmp_timeline = [], []
+    for i in tmp:
+        curr_value = float(i['value'].strip().replace(",", "."))
+        tmp_value.append(curr_value)
+        tmp_timeline.append(i['data'])
+    return tmp_value, tmp_timeline
+    
+    
+# Plot the charts
+plt.subplot(2, 2, 1)
+plt.plot(makeChart(eur_pln)[1], makeChart(eur_pln)[0], color='r', label='eur_pln')
+
+plt.subplot(2, 2, 2)
+plt.plot(makeChart(usd_pln)[1], makeChart(usd_pln)[0], color='g', label='usd_pln')
+
+plt.subplot(2, 2, 3)
+plt.plot(makeChart(eur_usd)[1], makeChart(eur_usd)[0], color='b', label='eur_usd')
+
+plt.legend()
+plt.show()
---
+++ data/spider.py
@@ -0,0 +1,19 @@
+import scrapy
+from datetime import date
+from scrapy.http import Response, Request
+
+
+class SpiderSpider(scrapy.Spider):
+    name = 'Spider'
+    allowed_domains = ['https://notowania.pb.pl']
+    start_urls = ['https://notowania.pb.pl/instrument/PBWALEUR/euro',
+                  'https://notowania.pb.pl/instrument/PBWALUSD/dolar',
+                  'https://notowania.pb.pl/instrument/PBWALEURUSD/eurusd']
+
+    def parse(self, response):
+        section = response.css('#pageMainContainer975')
+        yield {
+            'title': section.css('span.profilHead::text').get(),
+            'value': section.css('div.profilLast::text').get(),
+            'data': date.today()
+        }
