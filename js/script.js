c8ecb9551f4eadf41a7e096b78aadadbc265206c
g0gab1s <83614025+GabrielaGodek@users.noreply.github.com>
2022-12-31 23:49:38 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
 
+const eur_pln = document.querySelector('.statistic-page .title-radio input#eur_pln')
+const usd_pln = document.querySelector('.statistic-page .title-radio input#usd_pln')
+const eur_usd = document.querySelector('.statistic-page .title-radio input#eur_usd')
+
+
+document.querySelector('main.statistic-page').style.height = document.querySelector('.statistic-page .charts .eur_pln img').scrollHeight + 'px'
+
+document.querySelectorAll('.statistic-page .title-radio input').forEach(radio => {
+    radio.addEventListener('click', () => {
+        if (eur_pln.checked) {
+            document.querySelector('.statistic-page .charts .eur_pln').style.maxHeight = document.querySelector('.statistic-page .charts .eur_pln').scrollHeight + 'px'
+        } else {
+            document.querySelector('.statistic-page .charts .eur_pln').style.maxHeight = '0'
+        }
+
+        if (usd_pln.checked) {
+            document.querySelector('.statistic-page .charts .usd_pln').style.maxHeight = document.querySelector('.statistic-page .charts .usd_pln').scrollHeight + 'px'
+        } else {
+            document.querySelector('.statistic-page .charts .usd_pln').style.maxHeight = '0'
+        }
+
+        if (eur_usd.checked) {
+            document.querySelector('.statistic-page .charts .eur_usd').style.maxHeight = document.querySelector('.statistic-page .charts .eur_usd').scrollHeight + 'px'
+        } else {
+            document.querySelector('.statistic-page .charts .eur_usd').style.maxHeight = '0'
+        }
+    })
+})
+
